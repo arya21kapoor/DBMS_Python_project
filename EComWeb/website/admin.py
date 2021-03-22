e8557bed1890dd70f124109c76dea231a9016ca5
unknown <arya21kapoor@gmail.com>
2021-03-22 14:56:22 +0530

    Last change

--- EComWeb/website/admin.py
+++ EComWeb/website/admin.py
@@ -1,40 +1,43 @@
+# Registering models here.
 from django.contrib import admin
 from .models import Product, Category, OrderItem, Order, Payment, Coupon, Address
 from django.contrib.auth.models import Group
-# Register your models here.\
 
+# Below classes are for better display of the admin
+# Also for filtering according to the different fields
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price', 'discountPrice', 'category']
+    list_filter = ['category']
+
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ['item', 'quantity', 'done', 'user']
+    list_filter = ['done']
+
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ['user', 'amount', 'timestamp']
+    list_filter = ['user']
+
+class CouponAdmin(admin.ModelAdmin):
+    list_display = ['coupon_code', 'amount']
 
 class AddressAdmin(admin.ModelAdmin):
-    list_display = [
-        'user',
-        'address_1',
-        'address_2',
-        'city',
-        'state',
-        'zip',
-        'address_type',
-        'default',
-        'used'
-    ]
+    list_display = ['user', 'address_1', 'address_2', 'city', 'state', 'zip', 'address_type', 'default', 'used' ]
     list_filter = ['user', 'default', 'address_type']
-    # search_fields = ['user', 'default', 'address_type']
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = [
-        'user',
-        'payment',
-        'ordered',
-        'delivered',
-    ]
-    list_filter = ['user','ordered', 'delivered',]
-    # search_fields = ['user', 'ordered', 'delivered',]
+    list_display = ['user', 'payment', 'ordered', 'delivered']
+    list_filter = ['user','ordered', 'delivered']
 
-admin.site.register(Product)
-admin.site.register(Category)
-admin.site.register(OrderItem)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(OrderItem, OrderItemAdmin)
 admin.site.register(Order, OrderAdmin)
-admin.site.register(Payment)
-admin.site.register(Coupon)
+admin.site.register(Payment, PaymentAdmin)
+admin.site.register(Coupon, CouponAdmin)
 admin.site.register(Address, AddressAdmin)
 
-admin.site.unregister(Group)
+admin.site.unregister(Group) # not imp not now
